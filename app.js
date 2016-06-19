817ca30f2ad87bc2a38862a48d159b8c8eac80ef
noradium <yusuke_hosono@dwango.co.jp>
2016-06-19 14:57:53 +0900

    https対応

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render(err.message);
+  res.send(err.message);
 });
 
 
--- config/config.template.js
+++ config/config.template.js
@@ -1,6 +1,11 @@
 var config = {
     prod: {
-        port: '**PORT**'
+        port: '**PORT**',
+        ssl: {
+            key: '../cert/server.key',
+            cert: '../cert/server.crt',
+            ca: '../cert/ca.crt'
+        }
     },
     dev: {
         port: 3000
